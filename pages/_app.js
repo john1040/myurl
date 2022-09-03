94c73554b4895a57f0de5b079a103a7d74e938ea
johnwang <ilcf1u@gmail.com>
2022-09-02 18:59:32 -0700

    implemented basic image upload feature

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Head>
+        <title>myurl</title>
+      </Head>
+      <Component { ...pageProps } />
+    </ChakraProvider>
+    )
 }
 
 export default MyApp
